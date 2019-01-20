ae8a1e38de2e746cc8c5b0ce8371e212e8f2b1eb
GolovaVasilenko <vasilenko75@gmail.com>
2019-01-20 11:59:25 +0200

    start create design home page

--- webpack/src/js/custom.js
+++ webpack/src/js/custom.js
@@ -1,3 +1,32 @@
 jQuery(document).ready(function($) {
 
+    $("#form-registration").validate({
+        rules: {
+            login: {
+                required: true,
+                minlength: 2
+            },
+            password: {
+                required: true
+            },
+            confirm_password: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            login: {
+                required: 'Это поле обязательно для заполнения',
+                minlength: 'Поле должно содержать более 2 символов'
+            },
+            email: {
+                required: "Это поле обязательно для заполнения",
+                email: "Введите корректный формат E-mail"
+            }
+        }
+    });
+
 });
